f3573165eabea906e7f5920499f55683347bbbc6
Alex Alves <daoraalex@outlook.com>
2022-02-19 21:34:34 -0300

    Update classes iniciais

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Curso curso = new Curso(){{
+        Curso curso1 = new Curso(){{
             setTitulo("Curso Java");
             setDescricao("Descrição curso Java");
             setCargaHoraria(8);
         }};
 
-        System.out.println(curso);
+        Curso curso2 = new Curso(){{
+            setTitulo("Curso JS");
+            setDescricao("Descrição curso JS");
+            setCargaHoraria(4);
+        }};
+
+        Mentoria mentoria = new Mentoria(){{
+            setTitulo("Mentoria Java");
+            setDescricao("Descrição mentoria Java");
+            setData(LocalDate.now());
+        }};
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
 
     }
 
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -5,6 +5,9 @@ public class Curso {
     private String descricao;
     private int cargaHoraria;
 
+    public Curso() {
+    }
+
     public String getTitulo() {
         return titulo;
     }
